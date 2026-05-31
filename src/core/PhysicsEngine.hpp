bfcdb276298bf011f49be7861f196d183031e9bc
Saman <saman12382usp@gmail.com>
2026-05-31 15:20:39 +0330

    feat:  recursive fluid transfer logic passed

--- src/core/PhysicsEngine.hpp
+++ src/core/PhysicsEngine.hpp
@@ -25,4 +25,22 @@ public:
         
         return top1 == top2;
     }
+    // actualy moves the liquid and returns a new state copy for the ai  graph
+    static GameState do_pour(GameState state, int from_idx, int to_idx) {
+        // double check just in case
+        if(!can_pour(state, from_idx, to_idx)) return state; 
+
+        int moving_color = state.board[from_idx].colors.back();
+
+        // keep pouring the same color block as long as there is space
+        while(!state.board[from_idx].colors.empty() && 
+              state.board[from_idx].colors.back() == moving_color &&
+              state.board[to_idx].colors.size() < state.board[to_idx].max_cap) {
+            
+            state.board[from_idx].colors.pop_back();
+            state.board[to_idx].colors.push_back(moving_color);
+        }
+        
+        return state;
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
 #include "core/GameState.hpp"
+#include "core/PhysicsEngine.hpp" 
 
 using namespace std;
 
 int main() {
-    cout << "[SYSTEM] initilizing Entropy Engine..." << endl;
+    cout << "[SYSTEM] running physics tests  ..." << endl;
 
-    // quick check make sure structs compiles 
-    Flask f1;
-    f1.colors = {1, 1, 1, 1}; // pretend 1 is the color red
+    GameState s;
+    Flask f1; f1.colors = {1, 2, 2}; 
+    Flask f2; f2.colors = {1};       
+    Flask f3; f3.colors = {2};       
 
-    Flask f2;
-    f2.colors = {2, 2}; // half full of color 2
+    s.board.push_back(f1); 
+    s.board.push_back(f2); 
+    s.board.push_back(f3); 
 
-    if(f1.is_done()) {
-        cout << ">> Test pass: Flask 1 recognizes it is sorted." << endl;
+    // test 1: illegal move (pouring 2 onto 1)
+    if(PhysicsEngine::can_pour(s, 0, 1)) {
+        cout << ">> ERR: allowed an illegal pour." << endl;
     } else {
-        cout << ">> Test fail: Flask 1 logic is broken." << endl;
+        cout << ">> pass: blocked illegal pour." << endl;
     }
 
-    if(!f2.is_done()) {
-        cout << ">> Test pass: Flask 2 knows it is NOT sorted." << endl;
+    // test 2: legal move (pouring the two 2s onto the other 2)
+    GameState next_s = PhysicsEngine::do_pour(s, 0, 2);
+    
+    // f1 should now only have one item (the 1)  f3 should have three items (2, 2, 2)
+    if(next_s.board[0].colors.size() == 1 && next_s.board[2].colors.size() == 3) {
+        cout << ">> pass: liquids transfered correctly." << endl;
+    } else {
+        cout << ">> ERR: liquid math is broken." << endl;
     }
 
     return 0;
