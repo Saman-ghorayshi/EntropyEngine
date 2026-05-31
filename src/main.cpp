a86f59f6958801e64208875c383e4caca1b71426
Saman <saman12382usp@gmail.com>
2026-05-31 15:11:02 +0330

    test passed :)   Flask 1 recognizes it is sorted. Flask 2 knows it is NOT sorted.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include "core/GameState.hpp"
+
+using namespace std;
 
 int main() {
-    std::cout << "[SYSTEM] Entropy Engine Scaffolding Initialized." << std::endl;
+    cout << "[SYSTEM] initilizing Entropy Engine..." << endl;
+
+    // quick check make sure structs compiles 
+    Flask f1;
+    f1.colors = {1, 1, 1, 1}; // pretend 1 is the color red
+
+    Flask f2;
+    f2.colors = {2, 2}; // half full of color 2
+
+    if(f1.is_done()) {
+        cout << ">> Test pass: Flask 1 recognizes it is sorted." << endl;
+    } else {
+        cout << ">> Test fail: Flask 1 logic is broken." << endl;
+    }
+
+    if(!f2.is_done()) {
+        cout << ">> Test pass: Flask 2 knows it is NOT sorted." << endl;
+    }
+
     return 0;
 }
