d363d5c269b3b8dd74fb64c14714ca6374739d6f
Saman <saman12382usp@gmail.com>
2026-05-31 15:51:20 +0330

    feat: added raylib and render static gamestat

---
+++ include/ui/Renderer.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include "../core/GameState.hpp"
+#include <raylib.h>
+
+
+using namespace std;
+
+
+class Renderer {
+public:
+    // lazy way to map our int ids to actual colors
+    static Color get_color(int id) {
+        switch(id) {
+            case 1: return RED;
+            case 2: return BLUE;
+            case 3: return GREEN;
+            case 4: return YELLOW;
+            case 5: return PURPLE;
+            case 6: return ORANGE;
+            default: return DARKGRAY;
+
+        }
+    }
+
+    // draw a single bottle at a specific x,y coordinate
+    static void draw_flask(const Flask& f, int x, int y) {
+        int w = 60;   //  bottle width
+        int h = 200;  //   bottle height
+        int layer_h = h / f.max_cap; // height of one liquid block
+
+        // draw fluids from bottom to top
+        for(int i = 0; i < f.colors.size(); i++) {
+            Color c = get_color(f.colors[i]);
+            // math to stack them from the bottom of the bottle
+            int rect_y =  y +h -  ((i + 1 )  *  layer_h);
+            DrawRectangle(x,rect_y, w , layer_h, c );
+        }
+
+        // draw the glass outline 
+        DrawRectangleLinesEx({(float)x, (float)y, (float)w, (float)h}, 4, RAYWHITE);
+    }
+
+    // loops through the matrix and draws everything side by side
+    static void draw_board(const GameState& s) {
+        int start_x = 100;
+        int y = 150;
+        int spacing = 100; // space between bottles
+
+        for(int i = 0; i < s.board.size(); i++) {
+            draw_flask(s.board[i], start_x + (i * spacing), y);
+        }
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,37 @@
 #include <iostream>
+#include <raylib.h>
 #include "core/GameState.hpp"
-#include "core/PhysicsEngine.hpp"
 #include "core/LevelGen.hpp"
-#include "ai/Solver.hpp" 
+#include "ui/Renderer.hpp" 
 
 using namespace std;
 
-void print_board(const GameState& s) {
-    cout << "\n=== CURRENT BOARD ===" << endl;
-    for(int i = 0; i < s.board.size(); i++) {
-        cout << "Flask " << i << ": [ ";
-        for(int c : s.board[i].colors) {
-            cout << c << " ";
-        }
-        cout << "]" <<endl;
-    }
-    cout << "=========================\n"<< endl;
-}
-
 int main() {
-    cout << "[SYSTEM] Entropy Engine AI Mode Booting..."  <<  endl;
-    
-    // making a bit smaller level so the basic BFS doesnt take 10 minutes 
-    cout << "Generating puzzle..." << endl;
-    GameState s = LevelGen::make_level(4, 2,20); 
-    
-    print_board(s);
+    cout << "[SYSTEM] Booting graphical mode..." << endl;
 
-    cout << "[AI] Dispatching BFS Graph Solver..." <<endl;
-    vector<Move> win_path = Solver::solve_bfs(s);
+    // generate a puzzle 5 color, 2 empty,30 mix
+    GameState current_board = LevelGen::make_level(5,2,30);
 
-    if(!win_path.empty()) {
-        cout << ">> Optimal solution found in " << win_path.size() <<" moves:" << endl;
-        for(int i = 0; i < win_path.size(); i++) {
-            cout << "   Step " << i+1 << ": Pour Flask " << win_path[i].from << " -> Flask " << win_path[i].to << endl;
-        }
+    // start window
+    int screen_w = 900;
+    int screen_h = 500;
+    InitWindow(screen_w, screen_h, "Entropy Engine - Visual Test");
+    SetTargetFPS(60);
+
+    // standard game loop
+    while (!WindowShouldClose()) {
+        
+        BeginDrawing();
+        ClearBackground(GetColor(0x181818ff)); // dark backgroudn
+
+        // draw text
+        DrawText("Entropy Engine - Core Rendering Test", 20, 20, 20, LIGHTGRAY);
+        Renderer::draw_board(current_board);
+
+        EndDrawing();
     }
 
+    // clean up memo when press x
+    CloseWindow();
     return 0;
-    
 }
